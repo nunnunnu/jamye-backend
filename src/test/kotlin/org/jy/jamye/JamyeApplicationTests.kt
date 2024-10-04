259997113d8bb7459683edd3a23e3c0388b52498
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-10-04 21:31:22 +0900

    Initial commit

---
+++ src/test/kotlin/org/jy/jamye/JamyeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jy.jamye
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JamyeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
