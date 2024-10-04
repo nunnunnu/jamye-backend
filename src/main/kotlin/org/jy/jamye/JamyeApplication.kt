184fe71d5e10b43d4e4ba37abef60db34c34086b
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-10-04 21:31:22 +0900

    Initial commit

---
+++ src/main/kotlin/org/jy/jamye/JamyeApplication.kt
@@ -0,0 +1,11 @@
+package org.jy.jamye
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JamyeApplication
+
+fun main(args: Array<String>) {
+    runApplication<JamyeApplication>(*args)
+}
