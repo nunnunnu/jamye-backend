2f2b6f41ba8ae93f1957264cad82493fcef9599e
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-10-05 17:24:21 +0900

    String util class change

--- src/main/kotlin/org/jy/jamye/common/util/StringCustomUtils.kt
+++ src/main/kotlin/org/jy/jamye/common/util/StringCustomUtils.kt
@@ -1,13 +1,14 @@
 package org.jy.jamye.common.util
 
-import org.springframework.util.StringUtils.hasText
+import org.jy.jamye.common.util.StringUtils.isStringEmpty
 
-class StringCustomUtils {
-    companion object {
-        //문자열이 비어있으면 true
-        fun StringIsEmpty(data: String): Boolean {
-            return !hasText(data)
-        }
-
+object StringUtils {
+    /***
+     * 문자열의 blank or null 여부 검증
+     * @param 문자열
+     * @return 문자열이 blank/null 이면 false
+     */
+    fun isStringEmpty(data: String?): Boolean {
+        return data.isNullOrBlank()
     }
 }
