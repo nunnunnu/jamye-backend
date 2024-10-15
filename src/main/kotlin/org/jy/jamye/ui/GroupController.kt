d0b27040827ed4bc98d46e9c259fc2b654662e96
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-10-15 17:11:25 +0900

    그룹 초대코드 발급 api (#13)

--- src/main/kotlin/org/jy/jamye/ui/GroupController.kt
+++ src/main/kotlin/org/jy/jamye/ui/GroupController.kt
@@ -1,5 +1,6 @@
 package org.jy.jamye.ui
 
+import jakarta.servlet.http.HttpSession
 import org.jy.jamye.application.GroupApplicationService
 import org.jy.jamye.application.dto.GroupDto
 import org.jy.jamye.application.dto.UserInGroupDto
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/group")
-class GroupController(private val groupService: GroupApplicationService,) {
+class GroupController(private val groupService: GroupApplicationService,private val session: HttpSession) {
     @GetMapping("/list")
     fun groups(@AuthenticationPrincipal user: UserDetails) :  ResponseDto<List<GroupDto>> {
         val groups = groupService.getGroupsInUser(user.username)
@@ -36,5 +37,13 @@ class GroupController(private val groupService: GroupApplicationService,) {
     fun getGroup(@PathVariable("groupSeq") groupSeq: Long, @AuthenticationPrincipal user: UserDetails): ResponseDto<GroupDto.Detail> {
         val result = groupService.getGroup(user.username, groupSeq)
         return ResponseDto(data = result, status = HttpStatus.OK)
+    }
+
+    @PostMapping("/{groupSeq}/invite")
+    fun inviteGroupCode(@AuthenticationPrincipal user: UserDetails, @PathVariable groupSeq: Long): ResponseDto<String> {
+        val groupInviteCode: String = groupService.inviteGroupCode(user.username, groupSeq)
+        // redis 구현 전 임시 세션 저장
+        session.setAttribute("inviteCode", groupInviteCode)
+        return ResponseDto(data = groupInviteCode, status = HttpStatus.OK)
     }
 }
