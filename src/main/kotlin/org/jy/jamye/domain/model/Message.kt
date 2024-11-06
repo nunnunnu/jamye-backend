26c1a021defdb801167599e5f54948aea77da020
박진희 <110175918+nunnunnu@users.noreply.github.com>
2024-11-06 16:37:27 +0900

    post Detail entity 생성

---
+++ src/main/kotlin/org/jy/jamye/domain/model/Message.kt
@@ -0,0 +1,22 @@
+package org.jy.jamye.domain.model
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "pi_msg")
+@DiscriminatorValue("MSG")
+open class Message(
+    @Column(name = "pi-dtl")
+    var detail: String,
+    @Column(name = "nickName")
+    var nickName: String?,
+    @Column(name = "gu_seq")
+    var groupUserSequence: Long?,
+    @Column(name = "send_date")
+    var sendDate: LocalDateTime? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mi_seq")
+    var messageSeq: Long? = null
+) {
+}
